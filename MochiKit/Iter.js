eb977e80105b4855fa5b5f6df1b452f4aff6ea8c
Bob Ippolito <bob@redivi.com>
2005-07-23 07:50:31 +0000

    add typeof
    git-svn-id: http://svn.mochikit.com/mochikit/trunk@137 14cb8549-11fe-0310-83a5-45f3e82da484

--- MochiKit/Iter.js
+++ MochiKit/Iter.js
@@ -98,8 +98,7 @@ MochiKit.Iter.iter = function (iterable, /* optional */ sentinel) {
         return MochiKit.Iter.iteratorRegistry.match(iterable);
     } catch (e) {
         if (e == MochiKit.Base.NotFound) {
-            log('iterable.length =', MochiKit.Base.repr(iterable.length), typeof(iterable.length));
-            e = new TypeError(MochiKit.Base.repr(iterable) + " is not iterable");
+            e = new TypeError(typeof("iterable") + " " + MochiKit.Base.repr(iterable) + " is not iterable");
         }
         throw e;
     }
