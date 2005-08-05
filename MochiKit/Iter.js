21d04c30fe53323f0c4ea5c46fe5de293a069f0b
Bob Ippolito <bob@redivi.com>
2005-08-05 03:20:10 +0000

    Fix missing local
    git-svn-id: http://svn.mochikit.com/mochikit/trunk@239 14cb8549-11fe-0310-83a5-45f3e82da484

--- MochiKit/Iter.js
+++ MochiKit/Iter.js
@@ -319,6 +319,7 @@ MochiKit.Iter.imap = function (fun, p, q/*, ...*/) {
     ***/
     var map = MochiKit.Base.map;
     var iterables = map(MochiKit.Iter.iter, MochiKit.Base.extend(null, arguments, 1));
+    var next = MochiKit.Iter.next;
     return {
         "repr": function () { return "imap(...)"; },
         "toString": MochiKit.Base.forward("repr"),
