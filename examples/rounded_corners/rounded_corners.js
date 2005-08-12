ffce2f1ce6e610e2eb1d70f83052d7ac6a8eb674
Bob Ippolito <bob@redivi.com>
2005-08-12 05:42:06 +0000

    move syntax highlighting out to the common script
    git-svn-id: http://svn.mochikit.com/mochikit/trunk@255 14cb8549-11fe-0310-83a5-45f3e82da484

--- examples/rounded_corners/rounded_corners.js
+++ examples/rounded_corners/rounded_corners.js
@@ -4,37 +4,3 @@ var roundedCornersOnLoad = function () {
     roundClass("h2", null, {corners: "bottom"});
     syntaxHighlight();
 };
-
-var swapContents = function (dest, req) {
-    replaceChildNodes(dest, req.responseText);
-};
-
-var finishSyntaxHighlight = function () {
-    dp.sh.HighlightAll("code");
-    removeElementClass("codeview", "invisible");
-};
-
-var syntaxHighlight = function () {
-    var elems = getElementsByTagAndClassName("textarea", null, "codeview");
-    var dl = new Deferred();
-    var deferredCount = 0;
-    var checkDeferredList = function () {
-        deferredCount -= 1;
-        if (!deferredCount) {
-            dl.callback();
-        }
-    };
-    for (var i = 0; i < elems.length; i++) {
-        var elem = elems[i];
-        if (elem.name != "code") {
-            continue;
-        }
-        var url = strip(scrapeText(elem))
-        var d = doSimpleXMLHttpRequest(url).addCallback(
-            partial(swapContents, elem)
-        );
-        deferredCount += 1;
-        d.addCallback(checkDeferredList);
-    }
-    dl.addCallback(finishSyntaxHighlight);
-};
