89194afe6323da4e50779df0b51cda4d481b972c
Bob Ippolito <bob@redivi.com>
2005-08-13 23:43:52 +0000

    oops

--- examples/rounded_corners/rounded_corners.js
+++ examples/rounded_corners/rounded_corners.js
@@ -2,5 +2,4 @@ var roundedCornersOnLoad = function () {
     swapDOM("visual_version", SPAN(null, MochiKit.Visual.VERSION));
     roundClass("h1", null);
     roundClass("h2", null, {corners: "bottom"});
-    syntaxHighlight();
 };
