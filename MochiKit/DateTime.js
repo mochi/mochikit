be873fb52f6fa9223b3525b4c2f0f2e5c93e4ad8
Bob Ippolito <bob@redivi.com>
2005-10-20 22:37:38 +0000

    IE ifx?

--- MochiKit/DateTime.js
+++ MochiKit/DateTime.js
@@ -60,29 +60,29 @@ MochiKit.DateTime.isoTimestamp = function (str) {
     }
     var year, month, day, hour, min, sec, msec;
     year = parseInt(res[1], 10);
-    if (typeof(res[2]) == "undefined") {
+    if (typeof(res[2]) == "undefined" || res[2] == "") {
         return new Date(year);
     }
     month = parseInt(res[2], 10) - 1;
     day = parseInt(res[3], 10);
-    if (typeof(res[4]) == "undefined") {
+    if (typeof(res[4]) == "undefined" || res[4] == "") {
         return new Date(year, month, day);
     }
     hour = parseInt(res[4], 10);
     min = parseInt(res[5], 10);
-    sec = (typeof(res[6]) != "undefined") ? parseInt(res[6], 10) : 0;
-    if (typeof(res[7]) != "undefined") {
+    sec = (typeof(res[6]) != "undefined" && res[6] != "") ? parseInt(res[6], 10) : 0;
+    if (typeof(res[7]) != "undefined" && res[7] != "") {
         msec = Math.round(1000.0 * parseFloat("0." + res[7]));
     } else {
         msec = 0;
     }
-    if (typeof(res[8]) == "undefined" && typeof(res[9]) == "undefined") {
+    if ((typeof(res[8]) == "undefined" || res[8] == "") && (typeof(res[9]) == "undefined" || res[9] == "")) {
         return new Date(year, month, day, hour, min, sec, msec);
     }
     var ofs;
-    if (typeof(res[9]) != "undefined") {
+    if (typeof(res[9]) != "undefined" && res[9] != "") {
         ofs = parseInt(res[10], 10) * 3600;
-        if (typeof(res[11]) != "undefined") {
+        if (typeof(res[11]) != "undefined" && res[11] != "") {
             ofs += parseInt(res[11], 10) * 60;
         }
         if (res[9] == "-") {
