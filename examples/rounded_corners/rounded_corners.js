264d36706ce7011fe4659f70d5f75ecc65ea4244
Bob Ippolito <bob@redivi.com>
2005-10-24 02:06:27 +0000

    use view-source behavioral stuff
    git-svn-id: http://svn.mochikit.com/mochikit/trunk@351 14cb8549-11fe-0310-83a5-45f3e82da484

--- examples/rounded_corners/rounded_corners.js
+++ examples/rounded_corners/rounded_corners.js
@@ -3,3 +3,17 @@ var roundedCornersOnLoad = function () {
     roundClass("h1", null);
     roundClass("h2", null, {corners: "bottom"});
 };
+addLoadEvent(roundedCornersOnLoad);
+
+// rewrite the view-source links
+addLoadEvent(function () {
+    var elems = getElementsByTagAndClassName("A", "view-source");
+    var page = "rounded_corners/";
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        var href = elem.href.split(/\//).pop();
+        elem.target = "_blank";
+        elem.href = "../view-source/view-source.html#" + page + href;
+    }
+});
+
