9dcad9fdf820081f9e795ecb1efab05a3cf9b097
Bob Ippolito <bob@redivi.com>
2006-01-27 02:48:05 +0000

    workaround for IE workaround

--- MochiKit/MockDOM.js
+++ MochiKit/MockDOM.js
@@ -42,6 +42,13 @@ MochiKit.MockDOM.MockElement = function (name, data) {
         this.nodeType = 1;
         this.childNodes = [];
     }
+    if (name.substr(0, 1) == "<") {
+        var nameattr = name.substr(
+            name.indexOf('"') + 1, name.lastIndexOf('"'));
+        name = name.substr(1, name.indexOf(" "));
+        this.nodeName = name.toUpperCase();
+        this.setAttribute("name", nameattr);
+    }
 };
 
 MochiKit.MockDOM.MockElement.prototype = {
