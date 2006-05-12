92dd64f1711624c2ad407857974e3d0147417d2e
Thomas Herve <therve@gmail.com>
2006-05-12 09:35:49 +0000

    Update tests for DragAndDrop

--- tests/test_DragAndDrop.js
+++ tests/test_DragAndDrop.js
@@ -13,11 +13,18 @@ tests.test_DragAndDrop = function (t) {
     t.is(hasElementClass('drop1', 'drop-hover'), false, "remove hoverclass ok");
     drop1.destroy();
     
+    t.is( isEmpty(MochiKit.DragAndDrop.Droppables.drops), true, "Unregister droppable ok");
+    
     var onhover = function (element) {
         t.is(element, getElement('drag1'), 'onhover ok');
     };
     var drop2 = new MochiKit.DragAndDrop.Droppable('drop1', {'onhover': onhover});
-    var pos = elementPosition('drop1');
-    pos = [pos.x + 5, pos.y + 5];
-    MochiKit.DragAndDrop.Droppables.show(pos, getElement('drag1'));
+    var pos = getElementPosition('drop1');
+    pos = {"x": pos.x + 5, "y": pos.y + 5};
+    MochiKit.DragAndDrop.Droppables.show({"page": pos}, getElement('drag1'));
+
+    drag1.destroy();
+    t.is( isEmpty(MochiKit.DragAndDrop.Draggables.drops), true, "Unregister draggable ok");
+    
 };
+
