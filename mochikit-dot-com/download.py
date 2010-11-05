0f66fd25482519460ad61705a1de4ad18f345998
Per Cederberg <cederberg@gmail.com>
2010-11-05 09:11:55 +0100

    Added support for compressed download in MochiKit app engine helper.

--- mochikit-dot-com/download.py
+++ mochikit-dot-com/download.py
@@ -2,14 +2,33 @@ __version__ = '1'
 
 import sys
 import cgi
+import gzip
+import StringIO
+
+def compressData(data):
+    zbuf = StringIO.StringIO()
+    zfile = gzip.GzipFile(None, 'wb', 9, zbuf)
+    zfile.write(data)
+    zfile.close()
+    return zbuf.getvalue() 
 
 def main():
-    data = cgi.parse_qs(sys.stdin.read()).get('data')[0]
-    print 'Content-Type: text/plain'
-    print 'Content-Length: %d' % (len(data),)
-    print 'Content-Disposition: attachment; filename=MochiKit.js'
+    form = cgi.parse_qs(sys.stdin.read())
+    data = form.get('data')[0]
+    if 'compress' in form:
+        data = compressData(data)
+        print 'Content-Type: application/octet-stream'
+        print 'Content-Length: %d' % len(data)
+        if form['compress'][0] == 'gz':
+            print 'Content-Disposition: attachment; filename=MochiKit.js.gz'
+        else:
+            print 'Content-Disposition: attachment; filename=MochiKit.js.RENAME-TO-gz'
+    else:
+        print 'Content-Type: text/plain'
+        print 'Content-Length: %d' % len(data)
+        print 'Content-Disposition: attachment; filename=MochiKit.js'
     print ''
-    print data
+    sys.stdout.write(data)
 
 if __name__ == '__main__':
     main()
