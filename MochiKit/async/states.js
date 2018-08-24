9ea9de7c43feccd694ae87d87d9a8b56b8f3ca30
w0298i <wh0298i@yahoo.com>
2018-08-24 23:23:42 +0100

    Added new Deferred.js module with more p-zazz

---
+++ MochiKit/async/states.js
@@ -0,0 +1,11 @@
+const PENDING = 'pending';
+const RESOLVED = 'resolved';
+const REJECTED = 'rejected';
+const SETTLED = 'settled';
+
+export {
+    PENDING,
+    RESOLVED,
+    REJECTED,
+    SETTLED
+};
