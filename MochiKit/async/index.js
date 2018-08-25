e05847a45c90b9e3c55b0d827cfbafc4c694db70
w0298i <wh0298i@yahoo.com>
2018-08-25 15:47:41 +0100

    Removed operators

---
+++ MochiKit/async/index.js
@@ -0,0 +1,39 @@
+/**
+ * This file is basically a "folder module",
+ * meaning all the files in the folder
+ * are being exported from this module,
+ * so you can do:
+ * @example
+ * import { all, chain, Deferred } from 'mochikit/async';
+ * 
+ * This should work with modern-day module bundlers
+ * like Rollup, because it uses a technique called
+ * 'tree shaking' which basically means only the needed
+ * modules are included in the bundles. 
+ */
+export { default as all } from './all';
+export { default as callLater } from './callLater';
+export { default as catchSilent } from './catchSilent';
+export { default as chain } from './chain';
+export { default as defer } from './defer';
+export { default as Deferred } from './Deferred';
+export { default as double } from './double';
+export { default as failAfter } from './failAfter';
+export { default as getArrayBuffer } from './getArrayBuffer';
+export { default as getBlob } from './getBlob';
+export { default as getForm } from './getForm';
+export { default as getJSON } from './getJSON';
+export { default as getText } from './getText';
+export { default as isAsync } from './isAsync';
+export { default as isPromise } from './isPromise';
+export { default as isThenable } from './isThenable';
+export { default as Message } from './Message';
+export { default as MessageEmitter } from './MessageEmitter';
+export { default as prevent } from './prevent';
+export { default as reject } from './reject';
+export { default as resolve } from './resolve';
+export { default as simpleXHR } from './simpleXHR';
+export { default as succeedAfter } from './succeedAfter';
+export { default as tap } from './tap';
+export { default as tapFinally } from './tapFinally';
+export { default as whenSettled } from './whenSettled';
