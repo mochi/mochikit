131b4c417a901162c28854bac43c765fb4156b21
w0298i <wh0298i@yahoo.com>
2018-08-25 20:58:02 +0100

    added animation class and changed index.js

---
+++ MochiKit/animation/animations.js
@@ -0,0 +1,7 @@
+//Got these from https://easings.net/
+export const easeInSine = 'cubic-bezier(0.47, 0, 0.745, 0.715);';
+export const easeOutSine = 'cubic-bezier(0.39, 0.575, 0.565, 1);';
+export const easeInOutSine = 'cubic-bezier(0.445, 0.05, 0.55, 0.95);';
+export const easeInQuad = 'cubic-bezier(0.55, 0.085, 0.68, 0.53);';
+export const easeOutQuad = 'cubic-bezier(0.25, 0.46, 0.45, 0.94);';
+export const 
