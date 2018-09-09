33463b85dff6df88ac0de42376df65d5a9c6e27d
w0298i <wh0298i@yahoo.com>
2018-09-09 23:40:07 +0100

    added asyncnet class

---
+++ MochiKit/asyncnet/ImageLoader.js
@@ -0,0 +1,31 @@
+export default class ImageLoader {
+    constructor() {
+        this.failed = new Set();
+        this.loaded = new Set();
+    }
+
+    load(imgurl, document, attributes) {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            let el = document.createElement('img', attributes);
+            el.onload = resolve;
+            el.onerror = reject;
+            el.url = imgurl;
+            el.addEventListener('load', () => self.loaded.add(imgurl));
+            el.addEventListener('error', () => self.failed.add(imgurl));
+        });
+    }
+
+    createLoader(imgurl, document, attributes) {
+        let self = this;
+        return function boundLoader() {
+            return self.load(imgurl, document, attributes);
+        };
+    }
+
+    loadAfterDOM(imgurl, document, attributes) {
+        let self = this;
+        document.addEventListener('DOMContentLoaded', () => self.load(imgurl, document, attributes));
+        return this;
+    }
+}
