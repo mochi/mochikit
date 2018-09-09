d17cadf2930eb427f872a3f283e6b1dc7e5253c9
w0298i <wh0298i@yahoo.com>
2018-09-09 10:03:19 +0100

    added support for string __repr__

--- MochiKit/repr/getRepr.js
+++ MochiKit/repr/getRepr.js
@@ -1,4 +1,10 @@
 export default function getRepr(object) {
-    let repr = object && object.__repr__;
-    return repr && typeof repr === 'function' && repr.call(object);
+    let repr = object && object.__repr__,
+        type = repr ? typeof repr : false;
+
+    return type === 'function'
+        ? repr.call(object)
+        : type === 'string'
+            ? repr
+            : null;
 }
