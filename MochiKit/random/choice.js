75e87e47161e795206e330ad9ad0c6860dfdfd04
w0298i <wh0298i@yahoo.com>
2018-09-09 22:50:51 +0100

    removed random module, might use blq's one instead

--- MochiKit/random/choice.js
+++
@@ -1,5 +0,0 @@
-export default function choice(seq, min = 0, max) {
-    max = max || seq.length;
-    let index = Math.random() * (max - min) + min;
-    return seq[index];
-}
--- MochiKit/random/randomRange.js
+++
@@ -1,8 +0,0 @@
-import choice from "./choice";
-import { range } from "../iter";
-
-export default function randomRange(start, stop, step = 0) {
-    for(let index = 0; index < stop; index += step) {
-
-    }
-}
